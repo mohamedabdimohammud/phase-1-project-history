cf9abd89c42f709caa748d706a74993f7998818d
mohamedabdimohammud <mmohammud@outlook.com>
2023-04-09 17:11:49 +0300

    Completed assignment

--- history.js
+++ history.js
@@ -6,10 +6,10 @@ const main = document.querySelector('main');
   
 // Define an object with URL properties
 const data = {
-homeUrl: "http://127.0.0.1:5500/home.html",
-newsUrl: "http://127.0.0.1:5500/news.html",
-squadUrl: "http://127.0.0.1:5500/squad.html",
-storeUrl: "http://127.0.0.1:5500/store.html"
+homeUrl: "https://mohamedabdimohammud.github.io/phase-1-project/",
+newsUrl: "https://mohamedabdimohammud.github.io/phase-1-project-news/",
+squadUrl: "https://mohamedabdimohammud.github.io/phase-1-project-squad/",
+storeUrl: "https://mohamedabdimohammud.github.io/phase-1-project-store/"
 };
   
 // Add event listeners to buttons, using the URL properties from the data object
